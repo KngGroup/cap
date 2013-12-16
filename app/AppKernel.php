74a70b244161502458195b4e2a4b4945ef5e4211
bykov <bykov@mars.(none)>
2013-12-17 01:52:38 +0700

    fix app kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Sibers\ContactBundle\SibersContactBundle(),
-            new JMS\TranslationBundle\JMSTranslationBundle()
+            new JMS\TranslationBundle\JMSTranslationBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
