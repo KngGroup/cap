2260e534184d5db49100ff89df322eb57552cfd1
bykov <bykov@mars.(none)>
2013-12-17 00:54:45 +0700

    add fixtures

---
+++ src/Sibers/ContactBundle/DataFixtures/ORM/LoadContacts.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sibers\ContactBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sibers\ContactBundle\Entity\Contact;
+
+/**
+ * Description of LoadContacts
+ *
+ * @author bykov
+ */
+class LoadContacts implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $brian = new Contact();
+        $brian->setFirstname('Brian')
+              ->setFirstname('O`Conner')
+              ->setPhone('+19008005040');
+        
+        $manager->persist($brian);
+        
+        $dominic = new Contact();
+        $dominic->setFirstname('Dominic')
+                ->setLastname('Toretto')
+                ->setPhone('+19008001012');
+                
+        $manager->persist($dominic);
+        $manager->flush();
+    }
+
+}
